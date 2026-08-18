05d61903824879a290ff0cee1a203473cbc2b10e
xasanov <xasanovbekzod6261@gmail.com>
2026-08-18 18:50:44 +0500

    fix: prioritize unified request completion handler

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from config import BOT_TOKEN
 from handlers.user.start import router as start_router
 from handlers.user.requests import router as requests_router
 from handlers.admin.admins import router as admin_router
-from handlers.admin.requests import router as admin_requests_router
 from handlers.admin.finish import router as finish_router
+from handlers.admin.requests import router as admin_requests_router
 from handlers.admin.statistics import router as statistics_router
 
 
@@ -16,8 +16,8 @@ dp = Dispatcher()
 dp.include_router(start_router)
 dp.include_router(requests_router)
 dp.include_router(admin_router)
-dp.include_router(admin_requests_router)
 dp.include_router(finish_router)
+dp.include_router(admin_requests_router)
 dp.include_router(statistics_router)
 
 
