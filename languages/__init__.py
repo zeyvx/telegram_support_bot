aa04f32b1557e8b38e508c08fa92edcb5e1112a1
xasanov <xasanovbekzod6261@gmail.com>
2026-08-19 11:13:23 +0500

    Упростил получение переводов

--- languages/__init__.py
+++ languages/__init__.py
@@ -1,12 +1,22 @@
 from .ru import TEXTS as RU
 from .uz import TEXTS as UZ
 
-LANGUAGES = {'ru': RU, 'uz': UZ}
+LANGUAGES = {
+    'ru': RU,
+    'uz': UZ
+}
 
 
-def get_text(language, key, **kwargs):
-    texts = LANGUAGES.get(language, RU)
-    value = texts.get(key, RU.get(key, key))
-    if isinstance(value, str):
-        return value.format(**kwargs)
-    return value
+def get_text(language, key):
+    if language == 'uz':
+        texts = UZ
+    else:
+        texts = RU
+
+    if key in texts:
+        return texts[key]
+
+    if key in RU:
+        return RU[key]
+
+    return key
